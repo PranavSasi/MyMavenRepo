0c414e6cc964958a18fb45da8f99d4530593ea2d
KuAp <KuAp@LAPTOP-E8K2MQHG>
2023-07-24 12:57:34 +0530

    abcd

---
+++ MyMaven/src/main/java/helloWorld/OddOrEven.java
@@ -0,0 +1,15 @@
+package helloWorld;
+
+public class OddOrEven {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		int a=4;
+		if(a%2==0){
+			System.out.println("It is an Even number");
+		}
+		else{//
+			System.out.println("It is an Odd number");
+		}
+	}
+}
